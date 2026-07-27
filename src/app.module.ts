f97aa9fc46bed08b3a87ce46f46e8e43647da272
Hyeonseung Kang <hyeonseungkang@outlook.com>
2026-07-27 13:37:57 +0900

    feat: add .example.env file, update .gitignore, and integrate @nestjs/config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Location } from './location.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'data/locations.db',
-      entities: [],
+      entities: [Location],
       synchronize: true,
     }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
