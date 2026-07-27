5d4a29f03cc924a126181572f50416960042b7e8
Hyeonseung Kang <hyeonseungkang@outlook.com>
2026-07-27 13:37:35 +0900

    style: standardize import and string quotes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useBodyParser("json", { limit: "20mb" });
+  app.useBodyParser('json', { limit: '20mb' });
 
   const config = new DocumentBuilder().build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
